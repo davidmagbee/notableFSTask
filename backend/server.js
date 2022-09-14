02e08aa9b647c44281cbeaebeac92d2f63ded06c
magbee <david.magbee@me.com>
2022-09-14 15:09:44 -0400

    controller setup && switch

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
 const app = express();
+const controller = require('./controller')
 
-const data = require("./data")
-
+app.use(cors());
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
-    // GET ENDPOINT
-    res.send(data)
-})
+app.use('/', controller)
 
 app.listen(3001, () => console.log('👂🏼 listening... show me your data'));
-
